c9bd4be2cb67b03b2d0542966b06c04264c27296
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-09 07:14:34 +0530

    Update SGPA Calculator.c

--- SGPA Calculator.c
+++ SGPA Calculator.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
-//#include<conio.h>
 #include<stdlib.h>
 int main()
-#Take variables for each subject
+//Take variables for each subject
 {
     char s;int grade1,grade2,grade3,grade4,grade5,grade6;
    double SGPA=0.0,SGPA1=0.0;
    int sum,sum1;
-    #Ask the user which Cycle(P or C)
+
+    //Ask the user which Cycle(P or C)
     printf("Enter the current Cycle(Enter P-physics and C-chemistry)");
     scanf("%s",&s);
-    #Calculate the SGPA based on the grades
+
+    //Calculate the SGPA based on the grades
     if(s=='p' || s=='P')
     {
         printf("Enter the grade in EEE(In the range[1,10]) :");
